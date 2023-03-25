9f1b74379abb7bc185af7bef5b12d7ef261c23ce
Nikhil Sharma <103209278+Nikhil562@users.noreply.github.com>
2023-03-25 18:40:40 +0530

    Time: 135 ms (81.78%), Space: 60.5 MB (93.17%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.cpp
@@ -2,6 +2,19 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+//         intution:- we have 2 array and for each element we have to xor it with another array, and then xor all numbers so we finaly have each element that will be xor'ed to it self in the given number of another array.
+// now lets understand it with ex:-
+
+// example
+// lets say we have array [a,b] and [c,d,e] so according to question:
+
+// we will have numbers a^c , a^d, a^e, b^c, b^d , b^e;
+// final number will be a^c ^ a^d ^ a^e ^b^c^ b^d ^ b^e;
+// if we try to see pattern here a comes 3 times ,b 3 times c 2 times d 2 times and e 2 times which is nothing but the number of other array.
+
+// and by xor property we know if we xor any number to self even times it will become 0 or if we do odd times it will become itself
+
+// so now problem reduce to find the xor of all element in the array which have odd length which we can solve.
         int n1=nums1.size();
         int n2=nums2.size();
         int ans=0;
