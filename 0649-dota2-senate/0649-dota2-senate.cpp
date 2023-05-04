76b548e4869b81b2c50793761ccb6b57e4ccf825
Nikhil Sharma <103209278+Nikhil562@users.noreply.github.com>
2023-05-04 05:45:47 +0530

    Time: 9 ms (48.47%), Space: 8.1 MB (37.02%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+
+        queue<int> radiant, dire;
+        for(int i=0;i<n;i++){
+            if(senate[i] == 'R'){
+                radiant.push(i);
+            }
+            else{
+                dire.push(i);
+            }
+        }
+        if(radiant.size() == 0){
+            return "Dire";
+        }
+        else if(dire.size() == 0){
+            return "Radiant";
+        }
+        bool flag = true;
+
+        
+        while(!radiant.empty() && !dire.empty()){
+            if(radiant.front() < dire.front()){
+                flag = false;
+                dire.pop();
+                int r = radiant.front();
+                radiant.push(r+n);
+                radiant.pop();
+            }
+            else{
+                radiant.pop();
+                int d =  dire.front();
+                dire.pop();
+                dire.push(d+n);
+                flag = true;
+            }
+        }
+
+        return radiant.size() == 0 ? "Dire":"Radiant";
+        
+    }
+};
