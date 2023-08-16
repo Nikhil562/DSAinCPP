83dac457e34d2f15ab349c5b813911f162136afb
Nikhil Sharma <103209278+Nikhil562@users.noreply.github.com>
2023-08-17 02:32:19 +0530

    Added solution - LeetHub

--- Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.cpp
@@ -16,7 +16,7 @@ class Solution
 int shortestCommonSupersequence(string X, string Y, int m, int n)
     {
         //code here
-        vector<vector<int>>dp(m+1, vector<int>(n+1));
+        vector<vector<int>>dp(m+1, vector<int>(n+1,0));
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
