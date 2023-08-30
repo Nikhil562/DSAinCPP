1f1d52d66447c113717baaede2fb24828e9f63e9
Nikhil Sharma <103209278+Nikhil562@users.noreply.github.com>
2023-08-31 04:30:47 +0530

    Time: 46 ms (46.62%), Space: 7.8 MB (34.18%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -1,40 +1,26 @@
-// Count the frequency of each characters. Those characters with frequency greater than N / 4 must be replaced. Find the minimum window which covers those characters that should be replaced.
-
-// OJ: https://leetcode.com/problems/replace-the-substring-for-balanced-string/
-// Author: github.com/lzl124631x
-// Time: O(N)
-// Space: O(1)
 class Solution {
 public:
     int balancedString(string s) {
-        int N = s.size(), ans = N, i = 0, j = 0, replace = 0;
-        unordered_map<char, int> m;
-        for (char c : s) m[c]++;
-        for (auto &[c, cnt] : m) {
-            if ((m[c] = max(0, cnt - N / 4)) > 0) ++replace;
+        int n = s.size();
+        int k = n/4;
+        unordered_map<char,int> mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
         }
-        if (replace == 0) return 0;
-        while (j < N) {
-            // replace -= --m[s[j++]] == 0;
-            char currentChar = s[j];
-            int currentFreq = --m[currentChar];
-            // cout<<currentFreq<<endl;
-            if (currentFreq == 0) replace--;
-            j++;
-
-            while (replace <= 0) {
-                ans = min(ans, j - i);
-                // replace += m[s[i]]++ == 0;
-                char currentChar = s[i];
-                int originalFreq = m[currentChar];
-                if (originalFreq == 0) {
-                 replace++;
-                    }   
-                m[currentChar]++;
-
+        int i = 0;
+        int j = 0;
+        int ans = 1e9;
+        while(j<n){
+            mp[s[j]]--;
+            while(i<n && mp['W']<=k && mp['R']<=k && mp['E']<=k && mp['Q']<=k){
+                // i am tring to find the freq of W R E Q outside the current window 
+                //if all the frequency is less than <= k then we can replace the string to a suitable substring
+                ans = min(ans,(j-i+1));
+                mp[s[i]]++;//remove the calculation of the silding window when i shrink the window 
                 i++;
             }
+            j++;
         }
-        return ans;
+        return  ans;
     }
 };
