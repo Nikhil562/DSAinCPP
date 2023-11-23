43245225548aa2e9b8f9b1b066f28dd7874e0933
Nikhil Sharma <103209278+Nikhil562@users.noreply.github.com>
2023-11-23 11:16:42 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.78%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.cpp
@@ -3,9 +3,9 @@ public:
     string toHex(int num) {
         if(num==0) return "0";
         unsigned n=num;
-        string res;
+        string res="";
         char digits[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-        while(n>0){
+        while(n){
             res=(digits[n%16])+res;
             n/=16;
         }
